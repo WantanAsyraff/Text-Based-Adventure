57a2dbbf05056cb1d4fa3915d54123350505b61c
WantanAsyraff <147963245+WantanAsyraff@users.noreply.github.com>
2023-10-17 19:02:24 +0800

    Update
    Adjusted 5 files

--- EventHandler.py
+++ EventHandler.py
@@ -2,7 +2,6 @@ import EncounterSelect
 import random
 import time
 
-EventList = ["EnemyEncounter", "Loot", "Trap"]
 Enemy = EncounterSelect.Enemy
 CombatMode = False
 
@@ -18,23 +17,20 @@ def Loot():
     time.sleep(1)
 
     Rejuvinators = random.randint(0, 3)  # Change the range to include 3 as well
-    from RoamMode import Player
 
     if Rejuvinators == 0:
-        Player['HP'] += random.randint(6, 16)
+        HP = random.randint(6, 16)
         print("HP increased!")
     elif Rejuvinators == 1:
-        Player['DEF'] += random.randint(6, 16)
+        DEF = random.randint(6, 16)
         print("DEF increased!")
     elif Rejuvinators == 2:
-        Player['ATTACK'] += 4
+        ATTACK = 4
         print("ATTACK increased!")
     elif Rejuvinators == 3:
-        Player['SPEED'] += random.randint(6, 16)
+        SPEED = random.randint(6, 16)
         print("SPEED increased!")
-    
-    time.sleep(1)
-    breakpoint
+    return HP, DEF, ATTACK, SPEED
 
 
 def EnemyEncounter():
@@ -42,15 +38,16 @@ def EnemyEncounter():
     global CombatMode
     CombatMode = True
 
+
 def RandomEvent():
     
-    ChooseEvent = random.choice(EventList)
-    
-    if ChooseEvent == "Enemy Encounter":
+    ChooseEvent = random.randint(0, 1)
+
+    if ChooseEvent == 0:
         print("Enemy Encounter")
         EnemyEncounter()
     
-    elif ChooseEvent == "Loot":
+    elif ChooseEvent == 1:
         print("Loot")
         Loot()
         
--- RoamMode.py
+++ RoamMode.py
@@ -10,8 +10,7 @@ EventRate = 0.1
 EventOccurence = 0.5
 Progress = 30
 
-Floors = ["Level 0: Ground Floor", "Level 1: Basement", "Level 2: Hidden Laboratory", "Level 3: Confidential Experimental Department",
-          "Level 4: Control Room", "Level 5: War Preparations Room"]
+Floors = ["Level 0: Ground Floor", "Level 1: Basement", "Level 2: Hidden Laboratory", "Level 3: Confidential Experimental Department", "Level 4: War Preparations Room"]
 
 def FloorStatus():
     print("-------------------------------------------------------------||")
@@ -20,41 +19,48 @@ def FloorStatus():
     print("-------------------------------------------------------------||")
     time.sleep(1)
 
-FloorStatus()
+def Roaming():
+    global Steps
+    global EventRate
 
-while Steps < Progress:
-    
-    print("-------------------------------------------------------------||")
-    print(f"Current steps: {Steps} \nType 'walk' to advance")
-    print("-------------------------------------------------------------||")
-    PChoice = str(input(">>> "))
-    
-    if PChoice.lower() == "status":
-        print(f"Partner's status: {Player}")
-    
-    elif PChoice.lower() == "walk":
-        Steps += 1
-        EventRate += random.uniform(0.05, 0.10)
-        EventRate = round(EventRate, 2)
+    while Steps < Progress:
         
         print("-------------------------------------------------------------||")
-        print("Walking..")
-        time.sleep(0.3)
-        print("Walking...")
-        time.sleep(0.2)
-        print(EventRate)
+        print(f"Current steps: {Steps} \nType 'walk' to advance")
+        print("-------------------------------------------------------------||")
+        PChoice = str(input(">>> "))
         
-        if EventRate > EventOccurence:
-            EventHandler.RandomEvent()
-            EventRate = 0.1
-            print(f"resetted the event rate: {EventRate}")
-            breakpoint
-    
-    
+        if PChoice.lower() == "status":
+            print(f"Partner's status: {Player}")
+        
+        elif PChoice.lower() == "walk":
+            Steps += 1
+            EventRate += random.uniform(0.05, 0.10)
+            EventRate = round(EventRate, 2)
+            
+            print("-------------------------------------------------------------||")
+            print("Walking..")
+            time.sleep(0.3)
+            print("Walking...")
+            time.sleep(0.2)
+            print(EventRate)
+            
+            if EventRate > EventOccurence:
+                EventHandler.RandomEvent()
+
+                if EventHandler.RandomEvent() == 1:
+                    EventHandler.Loot()
+                    print(EventHandler.Loot)
+                    
+                EventRate = 0.1
+                print(f"resetted the event rate: {EventRate}")
     else:
         print("I don't quite get you...")
         print("-------------------------------------------------------------||")
-        
+
+FloorStatus() 
+Roaming()
+
 print("Out of the loop!")
 Progress += 30
 print(Progress)
