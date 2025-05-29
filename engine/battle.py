b318db68ecfca3bcc56df6600c0b2a012b453e81
WantanAsyraff <147963245+WantanAsyraff@users.noreply.github.com>
2025-05-29 14:16:00 +0800

    updated battle.py

--- engine/battle.py
+++ engine/battle.py
@@ -1,11 +1,18 @@
 class Battle:
-    def __init__(self):
-        pass
+    def __init__(self, player, enemy, inventory):
+        self.player = player
+        self.enemy = enemy
+        self.inventory = inventory
     
-    def player_attack(self):
-        pass
+    def player_turn(self):
+        options = {
+            "Spellcast": lambda: self.player.spellcast(self.enemy)
+            "Use an Item": lambda: self.player.useItem()
+            "Check Environment": lambda: self.player.checkEnvironment()
+            "Converse with Opponent": lambda:  self.player.converseOpponent(self.enemy)
+        }
     
-    def enemy_attack(self):
+    def attack(self):
         pass
     
     def update_turn(self):
--- engine/menus.py
+++ engine/menus.py
@@ -4,21 +4,25 @@ class Menus:
         self.player_options = player_options
     
     def display_menu(self):
-        print(f"{self.title}")
+        print(f"{self.title}\n")
         for idx, option in enumerate(self.player_options, 1):
             print(f"[{idx}] {option}")
         
         while True:
             try:
                 player_choice = int(input("Enter choice: "))
-                if 1 <= player_choice <= len(player_options):
-                    print(f">> You selected: {player_options[player_choice - 1]}")
-                    return player_options[player_choice - 1]
+                if 1 <= player_choice <= len(self.player_options):
+                    print(f">> You selected: ✨ {self.player_options[player_choice - 1]}")
+                    return self.player_options[player_choice - 1]
                 else:
-                    print(f"Choose action [1-{len(player_options)}]")
+                    print(f"❌ Choose action [1-{len(player_options)}]")
             except ValueError:
-                print(f"Choose a valid action [1-{len(player_options)}]")
+                print(f"❌ Choose a valid action [1-{len(player_options)}]")
     
-player_options = ["Start", "New game", "Reset", "Action"]
-test = Menus("Test menu", player_options)
+player_options = ["Spellcast", "Use an Item", "Check environment", "Converse with Opponent"]
+test = Menus("What are you going to do?", player_options)
+test.display_menu()
+
+spells = ["Sidestep", "Scythe Strike", "Zweihander guard"]
+test = Menus("What spell are you going to cast?", spells)
 test.display_menu()
